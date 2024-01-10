2c0be824383a470157c75b8fd1d96e51e1054034
mohamedatef-SE <mohamedatefyoussef@gmail.com>
2024-01-10 14:38:53 +0200

    animated progress-bars

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+//Nav-Bar
+
 const navBar = document.querySelector('.fixed-pos');
 
 let prevScrollPos = window.scrollY;
@@ -16,3 +19,37 @@ window.onscroll = function () {
 
 }
 
+
+// About
+const aboutSection = document.getElementById('about');
+const progressBars = document.querySelectorAll('.progress-bar');
+
+function showProgress() {
+    progressBars.forEach(progressBar => {
+        const value = progressBar.getAttribute('data-progress');
+        progressBar.style.opacity = 1;
+        progressBar.style.width = `${value}%`;
+    })
+
+}
+function hideProgress() {
+    progressBars.forEach(progressBar => {
+        progressBar.style.opacity = '0';
+        progressBar.style.width = `0%`;
+    })
+
+}
+
+window.addEventListener('scroll', () => {
+    const sectionTopFarFromView = aboutSection.getBoundingClientRect().top;
+    const sectionBottomFarFromView = aboutSection.getBoundingClientRect().bottom;
+    const screenView = window.innerHeight;
+    if (sectionTopFarFromView < screenView / 4) {
+        showProgress();
+    } else {
+        hideProgress();
+    }
+    if (sectionBottomFarFromView < screenView / 2) {
+        hideProgress();
+    }
+})
